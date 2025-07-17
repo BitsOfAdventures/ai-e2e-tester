a169421c74ffb00664f1ef0ede4cd5b229f2a246
alexis <bitsofadventures@gmail.com>
2025-07-17 07:22:32 -0400

    setup logging

--- src/ai_e2e_tester/utils.py
+++ src/ai_e2e_tester/utils.py
@@ -1,5 +1,15 @@
+import logging
+
 import yaml
 
 def load_config(path="config.yaml"):
     with open(path, "r", encoding="utf-8") as f:
         return yaml.safe_load(f)
+
+def setup_logging():
+    logger = logging.getLogger('ai-e2e-tester')
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter('%(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
