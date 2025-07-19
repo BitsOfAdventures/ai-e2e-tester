18e63f59c2d0afe3dbd7a4158e1a0301f6c02b97
alexis <bitsofadventures@gmail.com>
2025-07-19 11:13:19 -0400

    generating llm-compatible description of an action

--- src/ai_e2e_tester/browser/actions/browser_action.py
+++ src/ai_e2e_tester/browser/actions/browser_action.py
@@ -1,5 +1,6 @@
 import logging
 from abc import ABC
+from typing import Dict
 
 from playwright.sync_api import Page
 
@@ -7,7 +8,14 @@ logger = logging.getLogger('ai-e2e-tester.browser.actions')
 
 
 class BrowserAction(ABC):
-    name: str = 'llm_action_name'
+    # Name by which the LLM should call this action.
+    name: str = None
+
+    # The description will explain to the LLM when it should use this action.
+    description: str = None
+
+    # Describing to the LLM which input fields this action needs and how to format them.
+    input_fields: Dict[str, str] = {}
 
     def run(self, page: Page) -> str:
         """
@@ -16,3 +24,12 @@ class BrowserAction(ABC):
         :return: Description of the result of this action, in natural language. Will be given to LLM as feedback.
         """
         pass
+
+    @classmethod
+    def describe_for_llm(cls):
+        doc = f'- "{cls.name}": {cls.description}'
+        if cls.input_fields:
+            doc += " This field has following subfields:\n"
+            for k, v in cls.input_fields.items():
+                doc += f'    - "{k}": {v}\n'
+        return doc
