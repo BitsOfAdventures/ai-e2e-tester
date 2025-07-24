9b7291708ec2223184cc76ab36c487f2fadaf50a
alexis <bitsofadventures@gmail.com>
2025-07-24 08:11:59 -0400

    improved console reporter

--- src/ai_e2e_tester/reporting/console.py
+++ src/ai_e2e_tester/reporting/console.py
@@ -10,16 +10,20 @@ class ConsoleReporter:
     def __init__(self):
         self.console = Console()
 
+    def print_feedback_item(self, item: Dict[str, str]):
+        self.console.print(f"[bold cyan]{item['category']}[/] [bold]{item['name']}[/]")
+        self.console.print(f"{item['details']}\n")
+
+    def print_page(self, visited_page: VisitedPage):
+        for item in visited_page.feedback:
+            self.print_feedback_item(item)
+
     def print_report(self, grouped_visits: Dict[str, List[VisitedPage]]):
         """
         Prints a summary report of all visited pages.
         """
         for page_url, visited_pages in grouped_visits.items():
-            print(f'Page: {page_url}')
-            print('Bugs:')
-            [print(f'\t- {bug}') for bug in {bug for vp in visited_pages for bug in vp.bugs}]
-
-            print('Suggestions:')
-            [print(f'\t- {sg}') for sg in {sg for vp in visited_pages for sg in vp.suggestions}]
-
-            print('---\n')
+            self.console.print(f"\n[underline bold]Page:[/] {page_url}")
+            for visited_page in visited_pages:
+                self.print_page(visited_page)
+            self.console.print("---\n", style="dim")
